4c6b62ead511e5d4e9e23ecccf4a61bdfd70dda2
Jonathan Livingstone <jonathan.livingstone.56@gmail.com>
2024-08-04 21:51:00 +0100

    output directly to dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/destructured-products/' : '/',
   build: {
-    outDir: 'dist/client', // Adjust the output directory if needed
+    outDir: 'dist', // Output directly to dist
   },
   publicDir: 'public'
 });
